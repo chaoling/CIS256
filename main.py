a356ffcf90241236b97d9570a1f80e010fabd0e0
chao ling <chaol@chaos-MacBook-Pro-2.local>
2022-08-12 17:15:17 -0700

    added main function to test the function

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def caesarShift(txt: str, shift: int) -> str:
     pass
 
 
-assert caesarShift('example', 1) == 'fybnqmf', 'failed test case 1'
-assert caesarShift('example', -1) == 'dwzlokd', 'failed test case 2'
-assert caesarShift('python', 2) == 'ravjqp', "failed test case 3"
-assert caesarShift('pecan', 4) == 'tiger', "failed test case 4"
+if __name__ == "__main__":
+    txt = input()
+    shift = int(input())
+    print(caesarShift(txt, shift)
+
+    #assert caesarShift('example', 1) == 'fybnqmf', 'failed test case 1'
+    #assert caesarShift('example', -1) == 'dwzlokd', 'failed test case 2'
+    #assert caesarShift('python', 2) == 'ravjqp', "failed test case 3"
+    #assert caesarShift('pecan', 4) == 'tiger', "failed test case 4"
